5ecb929a441b9f63d2ffb05d917c4d92886da691
Kelly Jefferies <k.jefferies@gmail.com>
2022-04-23 15:27:58 -0500

    finishd model folder creation

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -1,5 +1,33 @@
 const { Schema, model } = require('mongoose');
-const moment=require('moment');
+const moment = require('moment');
+
+const reactionSchema = new Schema(
+    {
+        reactionID: {
+            type: Schema.Types.ObjectId(),
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => moment(createdAtVal).format('MMMM D, YYYY [at] hh:mm a')
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
 
 const thoughtsSchema = new Schema(
     {
@@ -17,8 +45,23 @@ const thoughtsSchema = new Schema(
         username: {
             type: String,
             required: true
-        }
-
-
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
     }
 );
+
+// create virtual property that gets the length of thoughts reactions array
+thoughtsSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+})
+
+const Thoughts = model('Thoughts', thoughtsSchema);
+
+module.exports = Thoughts;

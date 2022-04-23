b08d93481c15e52831e3c6c8157fb0cf6d3d24a5
Kelly Jefferies <k.jefferies@gmail.com>
2022-04-23 16:18:06 -0500

    create controller framework

---
+++ controllers/thought-controller.js
@@ -0,0 +1,27 @@
+const {Thoughts, Users} = require('../models');
+
+const thoughtController = {
+    getAllThoughts(req, res) {
+
+    },
+    getThoughtById({params}, res) {
+
+    },
+    createThought({params, body}, res) {
+
+    },
+    updateThought({params}, res) {
+
+    },
+    deleteThought({params}, res) {
+
+    },
+    createReaction({params, body}, res) {
+
+    },
+    deleteReaction({params}, res) {
+
+    }
+};
+
+module.exports = thoughtController;
---
+++ controllers/user-controller.js
@@ -0,0 +1,32 @@
+const Users = require('../models/Users');
+
+const userController = {
+    getAllUsers(req, res) {
+        try {
+
+        } catch (err) {
+
+        }
+
+    },
+    getUserId({ params }, res) {
+
+    },
+    createUser({ body }, res) {
+
+    },
+    updateUser({params, body}, res) {
+
+    },
+    deleteUser({params}, res) {
+
+    },
+    addFriendToUser({params}, res) {
+
+    },
+    deleteFriendFromUser({params}, res) {
+
+    }
+};
+
+module.exports = userController;
